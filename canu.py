6932da36a40b6d8943d0fc575e6d6e3edd3021f0
Quentin Canu <qcanu17@gmail.com>
2021-02-25 19:28:31 +0100

    Temps Idéal Voitures

--- canu.py
+++ canu.py
@@ -1,1 +1,14 @@
+from fistine import *
+
+def IdealTimeCars(D, N, M, nbCars, bonusPoint, real_streets, paths, streets_name):
+  "Durée Trajets idéal de chaque voiture"
+  IdealTimes = []
+  for i in range(nbCars):
+    duration = 0
+    for j in path:
+      duration += streets_name[2]
+    IdealTimes.append(duration)
+  return IdealTimes
+
+
 
