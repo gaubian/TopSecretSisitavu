534947bd7008490a735ca4eb8195de443628fc6f
Quentin Canu <qcanu17@gmail.com>
2021-02-25 19:35:27 +0100

    Ajout d'un dictionnaire streets_name

--- canu.py
+++ canu.py
@@ -1,5 +1,11 @@
 from fistine import *
 
+def streetNames(D, N, M, nbCars, bonusPoint, real_streets, paths):
+  streets_name = {}
+  for i in range(M):
+    streets_name[real_streets[2]] = [real_streets[0], real_streets[1], real_streets[3]]
+  return streets_name
+
 def IdealTimeCars(D, N, M, nbCars, bonusPoint, real_streets, paths, streets_name):
   "Durée Trajets idéal de chaque voiture"
   IdealTimes = []
