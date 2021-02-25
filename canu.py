28841f2257b729f47654f11e916baadf1f05d460
Quentin Canu <qcanu17@gmail.com>
2021-02-25 19:38:10 +0100

    FIxes de bugs

--- canu.py
+++ canu.py
@@ -12,7 +12,7 @@ def IdealTimeCars(D, N, M, nbCars, bonusPoint, real_streets, paths, streets_name
   for i in range(nbCars):
     duration = 0
     for j in path:
-      duration += streets_name[2]
+      duration += streets_name[j][2]
     IdealTimes.append((duration, bonusPoint + (D - duration))
   return IdealTimes
 
